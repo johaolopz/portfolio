3ccad259beb727396eb867b2b553750d88930725
johaolopz <johao.lopz.m@gmail.com>
2021-12-06 09:13:44 -0500

    Add Sidebar in _app.tsx / vsCode ext ES7, Headwind, Tailwind Css IntelliSense

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <div className="grid grid-cols-12 gap-6 px-48 my-14">
+    <div className="col-span-3 bg-white rounded-2xl">Sidebar</div>
+    <div className="col-span-9 bg-white rounded-2xl">
+      <Component {...pageProps} />
+    </div>
+  </div>
+  )
 }
 
 export default MyApp
