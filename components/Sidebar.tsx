a6067f269db46a10322427085a43dc28aee0cc67
johaolopz <johao.lopz.m@gmail.com>
2021-12-07 16:23:06 -0500

    Styles on SideBar, react-icons and classes tailwindcss

---
+++ components/Sidebar.tsx
@@ -0,0 +1,61 @@
+import Image from "next/image"
+import profilePhoto from "../images/profilePhoto.jpg"
+import {AiFillGithub, AiFillLinkedin} from "react-icons/ai"
+import {GoLocation} from "react-icons/go"
+import {FaLaptopCode} from "react-icons/fa"
+import {GiDiploma} from "react-icons/gi"
+
+const Sidebar = () => {
+    return (
+        <div>
+            <div className="w-32 h-32 mx-auto mb-8 rounded-full">
+                <Image
+                    src={profilePhoto}
+                    alt="profile photo"
+                    className="rounded-full"
+                />
+            </div>
+            <h3 className="my-4 text-3xl font-medium tracking-wider">
+                <span>Johao</span>
+                López
+            </h3>
+            <p className="flex items-center justify-center px-2 py-1 my-3 bg-gray-200 rounded-full">
+                <FaLaptopCode className="w-6 h-6" />
+                Full Stack Web Developer
+            </p>
+            <a 
+                href=""
+                download="name"
+                className="flex justify-center px-2 py-1 my-3 bg-gray-200 rounded-full"
+            >
+                Download Resume
+            </a>
+            {/* Social Icons */}
+            <div className="flex justify-around w-9/12 mx-auto my-5 text-green-500 md:w-full">
+                <a href="">
+                    <AiFillGithub className="w-8 h-8 cursor-pointer" />
+                </a>
+                <a href="">
+                    <AiFillLinkedin className="w-8 h-8 cursor-pointer" />
+                </a>
+                <a href="">
+                    <GiDiploma className="w-8 h-8 cursor-pointer" />
+                </a>
+            </div>
+            {/* address */}
+            <div className="py-4 my-5 bg-gray-200" style={{marginLeft:'-1rem', marginRight:'-1rem'}}>
+                <div className="flex items-center justify-center space-x-2">
+                    <GoLocation />
+                    <span>Guayaquil, Ecuador</span>
+                </div>
+                <p className="my-2">johao.lopz.m@gmail.com</p>
+                <p className="my-2">+593967667856 / +593959810663</p>
+            </div>
+            {/* Buttons */}
+            <button className="w-8/12 px-5 py-2 my-2 text-white rounded-full bg-gradient-to-r from-green-400 to-blue-400">Email me</button>
+            <button className="w-8/12 px-5 py-2 my-2 text-white rounded-full bg-gradient-to-r from-green-400 to-blue-400">Theme Mode</button>
+        </div>
+    )
+}
+
+export default Sidebar
