2633a8684a13c868d4b07544f267ed81505c2376
johaolopz <johao.lopz.m@gmail.com>
2021-12-10 00:07:00 -0500

    Creation of Service Cards component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,32 @@
 import { GetServerSidePropsContext, GetStaticPropsContext } from "next"
 import { services } from "../data"
+import ServiceCard from "../components/ServiceCard"
 
-const index = ({services}) => {
-  console.log(services)
+const index = () => {
   return (
-    <div>
-      <h1>NextJs Server Testing</h1>
+    <div className="flex flex-col flex-grow px-6 pt-1">
+      <h5 className="my-3 font-medium">
+        I am a Systems Engineer and Full Stack Web Developer. I highlight my soft
+        skills such as: communication, dedication, commitment, self-learning,
+        teamwork, among others. During the last year I have participated in the
+        development of responsive web applications with the PERN stack (PostgreSQL,
+        ExpressJs, React, NodeJs). I have both frontend and backend experience in
+        JavaScript environment. Although I have previously programmed in academic
+        projects with the free software package XAMPP. For a few months I was working
+        in Freelance mode as a Webmaster and SEO. I also have knowledge of wired and
+        wireless LAN administration, remote assistance and relationship with the client.
+      </h5>
+      <div className="flex-grow p-4 mt-5 bg-gray-400" style={{marginLeft:"-1.5rem", marginRight:"-1.5rem"}}>
+        <h6 className="my-3 text-xl font-bold tracking-wide">What I Offer</h6>
+        <div className="grid gap-6 lg:grid-cols-2">
+          {
+            services.map(service => (
+              <div className="bg-gray-200 rounded-lg lg:col-span-1">
+                <ServiceCard service={service} />
+              </div>))
+          }
+        </div>
+      </div>
     </div>
   )
 }
