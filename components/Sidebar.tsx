d6ce942debe09df5cba51ab173bdac143763e757
johaolopz <johao.lopz.m@gmail.com>
2021-12-12 11:37:03 -0500

    Theme button config and Light/Dark Mode styles with Tailwindcss

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,8 +4,15 @@ import {AiFillGithub, AiFillLinkedin} from "react-icons/ai"
 import {GoLocation} from "react-icons/go"
 import {FaLaptopCode} from "react-icons/fa"
 import {GiDiploma} from "react-icons/gi"
+import { useTheme } from "next-themes"
 
 const Sidebar = () => {
+    const { theme, setTheme} = useTheme()
+
+    const changeTheme = () => {
+        setTheme(theme === "light" ? "dark" : "light")
+    }
+
     return (
         <div>
             <div className="w-32 h-32 mx-auto mb-8 rounded-full">
@@ -19,14 +26,14 @@ const Sidebar = () => {
                 <span className="text-green">Johao</span>
                 López
             </h3>
-            <p className="flex items-center justify-center px-2 py-1 my-3 bg-gray-200 rounded-full">
+            <p className="flex items-center justify-center px-2 py-1 my-3 bg-gray-200 rounded-full dark:bg-dark-200 dark:bg-black-500">
                 <FaLaptopCode className="w-6 h-6" />
                 Full Stack Web Developer
             </p>
             <a 
                 href=""
                 download="name"
-                className="flex justify-center px-2 py-1 my-3 bg-gray-200 rounded-full"
+                className="flex items-center justify-center px-2 py-1 my-2 bg-gray-200 rounded-full cursor-pointer dark:bg-dark-200 dark:bg-black-500"
             >
                 Download Resume
             </a>
@@ -43,7 +50,7 @@ const Sidebar = () => {
                 </a>
             </div>
             {/* address */}
-            <div className="py-4 my-5 bg-gray-200" style={{marginLeft:'-1rem', marginRight:'-1rem'}}>
+            <div className="py-4 my-5 bg-gray-200 dark:bg-dark-200 dark:bg-black-500" style={{marginLeft:'-1rem', marginRight:'-1rem'}}>
                 <div className="flex items-center justify-center space-x-2">
                     <GoLocation />
                     <span>Guayaquil, Ecuador</span>
@@ -56,7 +63,12 @@ const Sidebar = () => {
                 className="w-8/12 px-5 py-2 my-2 text-white rounded-full bg-gradient-to-r from-green to-blue-400 focus:outline-none" 
                 onClick={()=>window.open('mailto:johao.lopz.m@gmail.com')}>
                 Email me</button>
-            <button className="w-8/12 px-5 py-2 my-2 text-white rounded-full bg-gradient-to-r from-green to-blue-400">Theme Mode</button>
+            <button 
+                className="w-8/12 px-5 py-2 my-2 text-white rounded-full bg-gradient-to-r from-green to-blue-400"
+                onClick={changeTheme}>
+                Theme Mode
+            </button>
+
         </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -17,14 +17,14 @@ const index = () => {
         in Freelance mode as a Webmaster and SEO. I also have knowledge of wired and
         wireless LAN administration, remote assistance and relationship with the client.
       </h5>
-      <div className="flex-grow p-4 mt-5 bg-gray-400" style={{marginLeft:"-1.5rem", marginRight:"-1.5rem"}}>
+      <div className="flex-grow p-4 mt-5 bg-gray-400 dark:bg-dark-100" style={{marginLeft:"-1.5rem", marginRight:"-1.5rem"}}>
         <h6 className="my-3 text-xl font-bold tracking-wide">What I Offer</h6>
         <div className="grid gap-6 lg:grid-cols-2">
           {
             services.map(service => {
               divKey++
               return (
-              <div key={divKey} className="bg-gray-200 rounded-lg lg:col-span-1">
+              <div key={divKey} className="bg-gray-200 rounded-lg dark:bg-dark-200 lg:col-span-1">
                 <ServiceCard service={service} />
               </div>)
             })
